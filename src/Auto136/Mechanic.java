d7f66199dbd5dbbf7bea0b15729b814b5c058358
Phap229 <133080131+Phap229@users.noreply.github.com>
2024-09-10 03:58:08 +0700

    Update Mechanic.java

--- src/Auto136/Mechanic.java
+++ src/Auto136/Mechanic.java
@@ -1,9 +1,11 @@
 package Auto136;
 
 import java.util.Date;
+
 public class Mechanic extends User {
-    public Mechanic(String fullName, Date dateOfBirth, String address,
-                    String phoneNumber, String email, boolean status) {
-        super(fullName, dateOfBirth, address, phoneNumber, email, User.UserType.MECHANIC, status);
+
+    public Mechanic(String userID, String fullName, Date dateOfBirth, String address, String phoneNumber, String email, boolean status) {
+        super(userID, fullName, dateOfBirth, address, phoneNumber, email, UserType.MECHANIC, status);
     }
+    
 }
