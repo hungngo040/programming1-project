b99be350c45d09127c57b0ff6127d1a37b103ecc
Phap229 <133080131+Phap229@users.noreply.github.com>
2024-09-10 03:57:48 +0700

    Update Manager.java

--- src/Auto136/Manager.java
+++ src/Auto136/Manager.java
@@ -1,9 +1,11 @@
 package Auto136;
 
 import java.util.Date;
+
 public class Manager extends User {
-    public Manager(String fullName, Date dateOfBirth, String address,
-                   String phoneNumber, String email, boolean status) {
-        super(fullName, dateOfBirth, address, phoneNumber, email, User.UserType.MANAGER, status);
+
+    public Manager(String userID, String fullName, Date dateOfBirth, String address, String phoneNumber, String email, boolean status) {
+        super(userID, fullName, dateOfBirth, address, phoneNumber, email, UserType.MANAGER, status);
     }
+    
 }
